c4de6ed611621397bbedb41d70688b403e15f902
Nina Yeh <ninayeh1211@gmail.com>
2020-01-03 10:46:43 +0000

    Update header

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,21 +1,28 @@
 import React from 'react'
 
-class Header extends React.Component {
-  render () {
-    return (
-      <div className="header">
-        <div className="grid">
-          <div className="start">
-            <a href="/">Home</a>
-          </div>
-          <div className="end">
-            <a href="/">Login</a>
-            <a href="/">Sign Up</a>
-          </div>
+const Header = (props) => {
+  return (
+    <div className="header">
+      <div className="grid">
+        <div className="start">
+          <a href="/">Home</a>
         </div>
+        <div className="end">
+          {props.nickname ? (
+            <span className="nickname">
+              <i className="far fa-user"></i>
+              {props.nickname}
+            </span>
+          ):(
+            <React.Fragment>
+              <a href="/">Login</a>
+              <a href="/">Sign Up</a>
+            </React.Fragment> 
+          )}
+        </div>
+      </div>
     </div>
-    )
-  }
+  )
 }
 
 export default Header;
