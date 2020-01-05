e0fc64762b0bc3cbcac40c4bd3e12db817c6a885
Nina Yeh <ninayeh1211@gmail.com>
2020-01-05 21:22:46 +0000

    Basic ToolBox Layout

--- src/component/ToolBox.js
+++ src/component/ToolBox.js
@@ -4,7 +4,21 @@ class ToolBox extends React.Component {
   render () {
     return (
       <div className="tool-box">
-        <p>Tool Box</p>
+        <div className="logo-text">Store</div>
+        <div className="search-box">
+          <div className="field has-addons">
+            <div className="contorl">
+              <input type="text" className="input search-input" placeholder="Search"/>
+            </div>
+            <div className="control">
+              <button className="button">X</button>
+            </div>
+          </div>
+        </div>
+        <div className="cart-box">
+          <i className="fas fa-shopping-cart"></i>
+          <span className="cart-number">(0)</span>
+        </div>
       </div>
     )
   }
