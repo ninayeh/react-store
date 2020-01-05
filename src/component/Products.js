3585003a682e26cf252776d3e7e1331d9c84ac56
Nina Yeh <ninayeh1211@gmail.com>
2020-01-05 21:12:22 +0000

    Add products page basic layout

--- src/component/Products.js
+++ src/component/Products.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import ToolBox from './ToolBox'
+import Product from './Product'
 
 class Products extends React.Component {
   render () {
     return (
-      <div className="products">
-        <p>Products Component</p>
-    </div>
+      <div>
+        <ToolBox />
+        <div className="products">
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+        </div>
+      </div>
     )
   }
 }
---
+++ src/component/ToolBox.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class ToolBox extends React.Component {
+  render () {
+    return (
+      <div className="tool-box">
+        <p>Tool Box</p>
+      </div>
+    )
+  }
+}
+
+export default ToolBox;
