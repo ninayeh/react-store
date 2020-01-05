8725654de4480252bf697341d977a9b290d71854
Nina Yeh <ninayeh1211@gmail.com>
2020-01-05 21:39:48 +0000

    Basic product card layout

--- src/component/Product.js
+++ src/component/Product.js
@@ -4,7 +4,22 @@ class Product extends React.Component {
   render () {
     return (
       <div className="product">
-        <p>Product</p>
+        <div className="p-content">
+          <div className="img-wrapper">
+            <figure className="image is-4by3">
+              <img src="images/2.jpg" alt=""/>
+            </figure> 
+            <p className="p-tags">25 colors</p>
+            <p className="p-name">Nike PG3</p>
+          </div>
+        </div>
+        <div className="p-footer">
+          <p className="price">38</p>
+          <button className="add-cart">
+            <i className="fas fa-shopping-cart"></i>
+            <i className="fas fa-exclamation"></i>
+          </button>
+        </div>
       </div>
     )
   }
