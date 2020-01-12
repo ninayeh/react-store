2735c9b5b85c13cbe3d3b06f4ab7c77f9dbe816b
Nina Yeh <ninayeh1211@gmail.com>
2020-01-12 17:13:12 +0000

    User can remove product from cart and see update total price

--- src/component/CartItem.js
+++ src/component/CartItem.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { formatPrice } from 'commons/helper';
-// import axios from 'commons/axios'
+import axios from 'commons/axios'
 // import { toast } from 'react-toastify';
 
 const CartItem = (props) => {
-  const {name, image, price, mount} = props.cart || {}
+  const [mount, setMount] = useState(props.cart.mount)
+  const {id, name, image, price} = props.cart || {}
   const sumPrice = formatPrice(parseInt(price) * mount)
+
+  const handleChange = e => {
+    const _mount = parseInt(e.target.value)
+    setMount(_mount)
+    const newCart = {
+      ...props.cart, // 拿到目前 props 裡面的 cart
+      mount: _mount
+
+    }
+    axios.put(`carts/${id}`, newCart).then(res => {
+      // 父組件裡面的函示
+      props.updateCart(newCart);
+    })
+    // console.log(newCart)
+  };
+
+  const deleteCart = () => {
+    axios.delete(`carts/${id}`).then(res => {
+      // 父組件裡面的函示
+      props.deleteCart(props.cart)
+    })
+  }
+
   return (
     <div className="columns is-vcentered">
       <div className="column is-narrow">
-        <span className="close">X</span>
+        <span className="close" onClick={deleteCart}>X</span>
       </div>
       <div className="column is-narrow">
         <img src={image} alt={name} width="100"/>
@@ -21,7 +45,13 @@ const CartItem = (props) => {
         <span className="price">{formatPrice(price)}</span>
       </div>
       <div className="column">
-        <input type="number" className="input num-input" defaultValue={mount}/>
+        <input 
+          type="number" 
+          min={1}
+          className="input num-input" 
+          value={mount}
+          onChange={handleChange}
+        />
       </div>
       <div className="column">
         <span className="sum-price">{sumPrice}</span>
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -11,14 +11,33 @@ const Cart = () => {
   const [carts, setCarts] = useState([]) //返回一個陣列（數組）
   
   // 類似於 componentDidAMount 那樣的生命週期函數
+  // 如果不傳遞第二個參數的話，會發現這個函式一直重複被執行
   useEffect(() => {
-    axios.get('/carts').then(res => setCarts(res.data))
-  })
+    console.log("===useEffect===")
+    axios.get('/carts').then(res => setCarts(res.data)); 
+  }, []);
 
   const totalPrice = () => {
+    console.log("===totalPrice===")
     const totalPrice = carts.map(cart => cart.mount * parseInt(cart.price))
     .reduce((a, value) => a + value, 0)
     return formatPrice(totalPrice)
+  }
+
+  // 更新 carts 陣列，為了讓他呼叫 totalPrice 重新計算金額
+  const updateCart = (cart) => {
+    const newCarts = [...carts];
+    const _index = newCarts.findIndex( c => c.id === cart.id);
+    newCarts.splice(_index, 1, cart)
+    setCarts(newCarts);
+  };
+
+  const deleteCart= (cart) => {
+    // 把收到的 cart 從 carts 陣列裡刪掉
+    // 如果 carts 陣列裡面的 id 不等於傳遞過來的 id，就保留
+    // 如此得到的新陣列 _carts 就會過濾掉不需要的 id
+    const _carts = carts.filter(c => c.id !== cart.id)
+    setCarts(_carts)
   }
 
   return(
@@ -27,7 +46,13 @@ const Cart = () => {
         <div className="cart-title">Shopping Cart</div>
         <div className="cart-list">
           {
-            carts.map(cart => (<CartItem key={cart.id} cart={cart} />))
+            carts.map(cart => (
+              <CartItem 
+                key={cart.id} 
+                cart={cart} 
+                updateCart={updateCart}
+                deleteCart={deleteCart}
+              />))
           }
           
         </div> 
