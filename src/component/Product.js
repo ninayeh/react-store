b00cf9810274bdc7896356d18f797c245dc5976c
Nina Yeh <ninayeh1211@gmail.com>
2020-01-12 11:45:16 +0000

    Add to cart

--- src/component/Product.js
+++ src/component/Product.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Panel from 'component/Panel'
 import {formatPrice} from 'commons/helper'
 import EditInventory from './EditInventory'
+import axios from 'commons/axios'
+import { toast } from 'react-toastify';
 
 class Product extends React.Component {
 
@@ -23,9 +25,45 @@ class Product extends React.Component {
     });
   };
 
+  addCart = async () => {
+    try {
+      const {id, name, image, price } = this.props.product;
+      // 查詢有沒有重複商品，異步函數
+      // GET /cart?id=1&id=2
+      const response = await axios.get(`/carts?productId=${id}`);
+      const carts = response.data;
+      console.log(carts);
+      if (carts && carts.length > 0){
+        const cart = carts[0]
+        cart.mount += 1
+        await axios.put(`/carts/${cart.id}`, cart)
+      } else {
+        const cart = {
+          productId: id,
+          name, 
+          image,
+          price,
+          mount: 1
+        };
+  
+        // 沒有重複商品的話就新增
+        // axios.post('/carts', cart).then( res => { 
+        //   toast.success('Add to cart!') 
+        // });
+        // 使用異步函數
+        await axios.post('/carts', cart)
+  
+      }    
+      toast.success('Add to cart!')  
+    } catch (error) {
+      toast.error('Add cart failed')  
+    }
+    
+  };
+
   
   render () {
-    const {name, image, tags, price, status} = this.props.product
+    const {name, image, tags, price, status} = this.props.product;
     const _pClass = {
       available: 'product',
       unavailable: 'product out-stock'
@@ -49,7 +87,7 @@ class Product extends React.Component {
         </div>
         <div className="p-footer">
           <p className="price">{formatPrice(price)}</p>
-          <button className="add-cart" disabled={status === 'unavailable'}> 
+          <button className="add-cart" disabled={status === 'unavailable'} onClick={this.addCart}> 
             <i className="fas fa-shopping-cart"></i>
             <i className="fas fa-exclamation "></i>
           </button>
